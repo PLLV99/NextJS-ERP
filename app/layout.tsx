7cc64d736a31790aeba0772014fcf19f5eebf033
PLLV99 <ppteacher2563@gmail.com>
2025-05-24 00:15:44 -0400

    firstday

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+      
+      <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" />
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
