f341f909df3a70be3d491b2d47c2f7f6963b35ca
PLLV99 <ppteacher2563@gmail.com>
2025-08-24 00:59:19 -0400

    Fix Webpack alias for @ path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
+// เพิ่ม alias '@' เพื่อให้สามารถใช้ import แบบย่อได้ เช่น import { ProductionInterface } from "@/app/interface/ProductionInterface";
